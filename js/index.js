8205572b6e5b78cfbeba22b27aa21612332b9654
dragonwong <814465104@qq.com>
2015-03-24 03:11:51 +0800

    animation done

--- js/index.js
+++ js/index.js
@@ -1,14 +1,52 @@
+var clickable = true
 var kana = avalon.define({
 	$id: 'kana',
 	isTurnOver: false,
-	swip: '',
+	isSwip: false,
+	isCur: false,
 
 	act: '',
-	turnOver: function() {
-		kana.isTurnOver = !kana.isTurnOver;
+	turnOver: function(index) {
+		var thisCard = kana.cards[index]
+
+		if (clickable) {
+			thisCard.isTurnOver = !thisCard.isTurnOver
+		}
 	},
-	swipeleft: function() {
-		kana.act = '向左划'
-		kana.swip = 'shuffle'
-	}
-});
+	swipe: function(index) {
+		clickable = false
+		var thisCard = kana.cards[index]
+
+		if (thisCard.isCur) {
+			thisCard.isSwipe = true
+
+			setTimeout(function() {
+				thisCard.isCur = false
+				thisCard.isTurnOver = false
+				thisCard.isSwipe = false
+				clickable = true
+			}, 2000)
+
+			setTimeout(function() {
+				var thatCard = kana.cards[1 - index]
+				thatCard.isCur = true
+			}, 1800)
+		}
+	},
+
+	cards: [{
+		isCur: false,
+		isTurnOver: false,
+		isSwipe: false,
+		data: {
+			name: 'A'
+		}
+	}, {
+		isCur: true,
+		isTurnOver: false,
+		isSwipe: false,
+		data: {
+			name: 'B'
+		}
+	}]
+})

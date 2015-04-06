3ade86a2b13aff75c55df839c116797bd0f3140d
dragonwong <814465104@qq.com>
2015-04-06 14:42:58 +0800

    增加浊音

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 var qingyin48 = 'あ,ア,a,い,イ,i,う,ウ,u,え,エ,e,お,オ,o,か,カ,ka,き,キ,ki,く,ク,ku,け,ケ,ke,こ,コ,ko,さ,サ,sa,し,シ,shi,す,ス,su,せ,セ,se,そ,ソ,so,た,タ,ta,ち,チ,chi,つ,ツ,tsu,て,テ,te,と,ト,to,な,ナ,na,に,ニ,ni,ぬ,ヌ,nu,ね,ネ,ne,の,ノ,no,は,ハ,ha,ひ,ヒ,hi,ふ,フ,fu,へ,ヘ,he,ほ,ホ,ho,ま,マ,ma,み,ミ,mi,む,ム,mu,め,メ,me,も,モ,mo,や,ヤ,ya,い,イ,i,ゆ,ユ,yu,え,エ,e,よ,ヨ,yo,ら,ラ,ra,り,リ,ri,る,ル,ru,れ,レ,re,ろ,ロ,ro,わ,ワ,wa,を,ヲ,wo,ん,ン,n',
 	zhuoyin20 = 'が,ガ,ga,ぎ,ギ,gi,ぐ,グ,gu,げ,ゲ,ge,ご,ゴ,go,ざ,ザ,za,じ,ジ,ji,ず,ズ,zu,ぜ,ゼ,ze,ぞ,ゾ,zo,だ,ダ,da,ぢ,ヂ,di,づ,ヅ,du,で,デ,de,ど,ド,do,ば,バ,ba,び,ビ,bi,ぶ,ブ,bu,べ,ベ,be,ぼ,ボ,bo',
-	banzhuoyin5;
+	banzhuoyin5 = 'ぱ,パ,pa,ぴ,ピ,pi,ぷ,プ,pu,ぺ,ペ,pe,ぽ,ポ,po';
 
 /**
  * get a array of kana
